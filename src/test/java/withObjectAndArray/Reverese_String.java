a5493940f6b0f90e75300f4c9e576fa8c106fa52
asus <asus@DESKTOP-HMSSTKG>
2022-09-06 15:11:15 +0530

    api

---
+++ src/test/java/withObjectAndArray/Reverese_String.java
@@ -0,0 +1,16 @@
+package withObjectAndArray;
+
+public class Reverese_String {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s="Lohith";
+		
+		for(int i=s.length()-1;i>=0;i--)
+	
+		{
+	    System.out.print(s.charAt(i));
+	}
+	}
+	}
